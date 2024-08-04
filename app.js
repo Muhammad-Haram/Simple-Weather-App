23a8e437759d7181f4332ee9d8b06197217eafe5
Muhammad Haram <haramh643@gmail.com>
2024-08-04 16:54:14 +0500

    done

--- app.js
+++
@@ -1,4 +0,0 @@
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
-const form = document.querySelector("form")
-const search = document.querySelector("#search")
-const weather = document.querySelector("#weather")
